58a3ca41cdfbf3d5651b0e9682c1c002c62be739
Ishaan-10 <b.ishaan10@gmail.com>
2022-04-23 12:16:24 +0530

    done

--- functions.js
+++ functions.js
@@ -1,58 +1,71 @@
 const Student = require('./models/student');
 const Teacher = require('./models/teacher');
 
-// const teacher = {
-//     name:"Ishaan",
-//     email:"b.ishaan10@gmail.com",
-//     subject:"Computer Science"
-// }
+
+const teacher = {
+    name:"Ishaan",
+    email:"b.ishaan10@gmail.com",
+    subject:"Computer Science"
+}
 const createTeacher = async (obj) =>{
     const {name,email,subject} = obj;
     const newTeacher = new Teacher({name,email,subject});
     await newTeacher.save();
+    console.log("Created new teacher")
 }
 
-// const student = {
-//     name:"Ishaan",
-//     email:"b.ishaan10@gmail.com",
-//     grade:9,
-//     section:"A"
-// }
 const createStudent = async (obj)=>{
-
     const {name,email,grade,section} = obj;
     const newStudent = new Student({name,email,grade,section});
     await newStudent.save();
-
+    console.log("Created new student")
 }
+
 
 const assignTeacherToStudent = async (student_id,teacher_id)=>{
     const teacher = await Teacher.findById(teacher_id);
     const student = await Student.findById(student_id);
     student.assignedTeacher = teacher;
     student.save();
+    console.log("Assigned student to teacher")
 }
 
 const getTeacher = async (subject) =>{
-    const teachers = await Teacher.find({subject})
+    const teachers = await Teacher.find({subject}).exec().then((result)=>{
+        return result;
+    })
+    console.log(teachers);
     return teachers;
 }
 
+
 const getStudents = async (grade,section) =>{
     const students = await Student.find({grade,section});
+    console.log(students)
     return students;
 }
+
 
 const editStudent = async (student_id,updatedObj)=>{
     await Student.findByIdAndUpdate(student_id,updatedObj);
+    console.log("Student updated")
 }
 
+
 const getAssignedStudentsToTeacher = async(teacher_id)=>{
-    const students = await Student.find({}).populate({
-        path:'teacherAssigned',
-        match:{
-            _id:teacher_id
-        }
-    });
-    return students;
+    const students = await Student.find({}).populate('assignedTeacher');
+    const filtered = students.filter(obj=>{
+        return obj.assignedTeacher?._id == teacher_id})
+    // console.log(students)
+    console.log(filtered)
+}
+
+module.exports = {
+    createTeacher,
+    createStudent,
+    assignTeacherToStudent,
+    getTeacher,
+    getStudents,
+    editStudent,
+    getAssignedStudentsToTeacher
 }
--- index.js
+++ index.js
@@ -1,8 +1,47 @@
-const express = require('express');
-const Student = require('./models/student');
-const Teacher = require('./models/teacher');
-const app = express();
+const {
+    createTeacher,
+    createStudent,
+    assignTeacherToStudent,
+    getTeacher,
+    getStudents,
+    editStudent,
+    getAssignedStudentsToTeacher} = require('./functions');
 
-app.listen(process.env.PORT || 3000,()=>{
-    console.log("Server running")
-})
+// Connectig db
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+mongoose.connect('mongodb://localhost:27017/student-teacher', { useNewUrlParser: true, useUnifiedTopology: true });
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("Database Connected")
+});
+
+
+const teacher = {
+    name:"Ishaan",
+    email:"b.ishaan10@gmail.com",
+    subject:"Computer Science"
+}
+// createTeacher(teacher);
+
+const student = {
+  name:"Ishaan",   
+  email:"b.ishaan10@gmail.com",
+  grade:9,
+  section:"A"
+}
+
+// createStudent(student);
+
+// assignTeacherToStudent("6263985ccf956be165396451","6263962e57e9b91c64a6430c");
+// getTeacher("Computer Science")
+// getStudents(9,"A");
+
+const updatedStudent = {
+  name:"Ishaan",   
+  email:"b.ishaan10@gmail.com",
+  grade:10,
+  section:"C"
+}
+// editStudent("62639b1f5bb77cd9c8d506ac",updatedStudent)
+getAssignedStudentsToTeacher("6263962e57e9b91c64a6430c")
